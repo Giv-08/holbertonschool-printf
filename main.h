fff51acad4c5fb1db995cd1eb12fac0e4c4faa45
Giv-08 <chutima.p8@gmail.com>
2024-07-24 10:06:11 +0000

    added prototype back in main

--- main.h
+++ main.h
@@ -21,6 +21,7 @@ void _putchar(char c);
 int _printf(const char *format, ...);
 void print_char(va_list args);
 void print_string(va_list args);
+void print_signed_int(int n);
 void print_int(va_list args);
 void print_percent(va_list args);
 void (*get_specifier(char s))(va_list);
