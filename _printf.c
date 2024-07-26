333e2ffb951ddbbe50272aa5aaf4aac4aeed11fb
Giv-08 <chutima.p8@gmail.com>
2024-07-26 05:24:40 +0000

    fixed betty at last

--- _printf.c
+++ _printf.c
@@ -1,50 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 
 /**
- * _printf - produces output according to a format.
- * @format: is a string with format specifiers.
- *
- * Return: the number of characters printed.
- *
+ * _printf - produces output according to a format
+ * @format: is a string with format specifiers
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int printed_chars = 0;
-	int count;
-       	int (*printf_sp)(va_list args);
+	int printed_chars = 0, count;
+	int (*printf_sp)(va_list args);
 
 	va_start(args, format);
 
 	if (format && strlen(format) == 1 && format[0] == '%')
-	{
-		return -1;
-	}
-
+		return (-1);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
 			if (*format == '\0')
-			{
 				break;
-			}
-
 			printf_sp = get_specifier(*format);
 
 			if (printf_sp)
 			{
 				count = printf_sp(args);
 				if (count > 0)
-				{
 					printed_chars = printed_chars + count;
-				}
 			}
-		       	else
+			else
 			{
 				_putchar('%');
 				_putchar(*format);
